3d19848dc96e1e7e3f0dfd54e0ba382ed36d285d
Saidziaa <134191888+Saidziaa@users.noreply.github.com>
2023-05-21 14:50:37 +0500

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`//dukingdraon.com/4/5969464`
+					`dukingdraon.com/4/5969464`
 				}`,
 			},
 		};
